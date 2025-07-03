c3c3cdd77010dd3e2884a972a7a399ffb64919c5
Irinaa <irifigueroa1408@gmail.com>
2025-07-02 21:12:59 -0300

    Add files via upload

---
+++ intronode/index.js
@@ -0,0 +1,34 @@
+import express from "express"
+
+const servidor=express()
+servidor.get("/publicacion",(req,res)=>{
+    res.json({
+        mensaje:"Bienvenido a la API de [searchjob]"
+    });
+});
+
+servidor.get("/publicaciones",(req,res)=>{
+    res.json({
+        mensaje:"Esta es la ruta [get] de mi entidad [publicaciones]"
+    });
+});
+
+servidor.post("/publicaciones",(req,res)=>{
+    res.json({
+        mensaje:"Esta es la ruta [post] de mi entidad [publicaciones]"
+    });
+});
+servidor.put("/publicaciones",(req,res)=>{
+res.json({
+        mensaje:"Esta es la ruta [put] de mi entidad [publicaciones]"
+    });
+});
+servidor.delete("/publicaciones",(req,res)=>{
+res.json({
+        mensaje:"Esta es la ruta [delete] de mi entidad [publicaciones]"
+    });
+});
+
+servidor.listen(3000,()=>{
+    console.log("el servidor express esta en el puerto 3000");
+})
