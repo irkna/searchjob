d40e63b186261f1a93617be543e646f61a737337
Irinaa <irifigueroa1408@gmail.com>
2025-08-06 13:38:22 -0300

    Add files via upload

---
+++ routes/usuarios.routes.js
@@ -0,0 +1,27 @@
+import { Router } from "express";
+const router = Router();
+
+router.get("/", (req, res) => {
+  res.json({ mensaje: "Esta es la ruta [get] de mi entidad [usuarios]" });
+});
+
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  res.json({ mensaje: `Esta es la ruta GET de mi entidad [usuarios] con el ID ${id}` });
+});
+
+router.post("/", (req, res) => {
+  res.json({ mensaje: "Esta es la ruta [post] de mi entidad [usuarios]" });
+});
+
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  res.json({ mensaje: `Esta es la ruta [put] de mi entidad [usuarios] con el ID ${id}` });
+});
+
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  res.json({ mensaje: `Esta es la ruta [delete] de mi entidad [usuarios] con el ID ${id}` });
+});
+
+export default router;
---
+++ server.js
@@ -0,0 +1,23 @@
+console.log(" 7°2 actividad numero 9");
+
+import express from "express";
+import publicacionesRoutes from "./routes/publicaciones.routes.js";
+import usuariosRoutes from "./routes/usuarios.routes.js";
+
+const servidor = express();
+
+servidor.use(express.json());
+
+// Rutas
+servidor.use("/publicaciones", publicacionesRoutes);
+servidor.use("/usuarios", usuariosRoutes);
+
+servidor.get("/publicacion", (req, res) => {
+  res.json({
+    mensaje: "Bienvenido a la API de [searchjob]",
+  });
+});
+
+servidor.listen(3000, () => {
+  console.log("El servidor express está en el puerto 3000");
+});
