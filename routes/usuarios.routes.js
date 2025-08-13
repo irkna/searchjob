701f61a87f90f078c689e9caab89c4e0df8eb64a
Irinaa <irifigueroa1408@gmail.com>
2025-08-13 19:08:52 -0300

    Update usuarios.routes.js

--- routes/usuarios.routes.js
+++ routes/usuarios.routes.js
@@ -1,27 +1,28 @@
 import { Router } from "express";
 const router = Router();
 
-router.get("/", (req, res) => {
+router.get("/usuarios", (req, res) => {
   res.json({ mensaje: "Esta es la ruta [get] de mi entidad [usuarios]" });
 });
 
-router.get("/:id", (req, res) => {
+router.get("/usuarios/:id", (req, res) => {
   const id = req.params.id;
   res.json({ mensaje: `Esta es la ruta GET de mi entidad [usuarios] con el ID ${id}` });
 });
 
-router.post("/", (req, res) => {
+router.post("/usuarios", (req, res) => {
   res.json({ mensaje: "Esta es la ruta [post] de mi entidad [usuarios]" });
 });
 
-router.put("/:id", (req, res) => {
+router.put("/usuarios/:id", (req, res) => {
   const id = req.params.id;
   res.json({ mensaje: `Esta es la ruta [put] de mi entidad [usuarios] con el ID ${id}` });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/usuarios/:id", (req, res) => {
   const id = req.params.id;
   res.json({ mensaje: `Esta es la ruta [delete] de mi entidad [usuarios] con el ID ${id}` });
 });
 
 export default router;
+
