6645ce1d75b6914fc00a26939aef9fbd4bc428cf
Irinaa <irifigueroa1408@gmail.com>
2025-08-13 19:09:40 -0300

    Update publicaciones.routes.js

--- routes/publicaciones.routes.js
+++ routes/publicaciones.routes.js
@@ -1,27 +1,28 @@
 import { Router } from "express";
 const router = Router();
 
-router.get("/", (req, res) => {
+router.get("/productos", (req, res) => {
   res.json({ mensaje: "Esta es la ruta [get] de mi entidad [publicaciones]" });
 });
 
-router.get("/:id", (req, res) => {
+router.get("/productos/:id", (req, res) => {
   const id = req.params.id;
   res.json({ mensaje: `Esta es la ruta GET de mi entidad [publicaciones] con el ID ${id}` });
 });
 
-router.post("/", (req, res) => {
+router.post("/productos", (req, res) => {
   res.json({ mensaje: "Esta es la ruta [post] de mi entidad [publicaciones]" });
 });
 
-router.put("/:id", (req, res) => {
+router.put("/productos/:id", (req, res) => {
   const id = req.params.id;
   res.json({ mensaje: `Esta es la ruta [put] de mi entidad [publicaciones] con el ID ${id}` });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/productos/:id", (req, res) => {
   const id = req.params.id;
   res.json({ mensaje: `Esta es la ruta [delete] de mi entidad [publicaciones] con el ID ${id}` });
 });
 
 export default router;
+
