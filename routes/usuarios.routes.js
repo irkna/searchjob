5f758d8872793af99845f43375d56f92bffc36c4
Irinaa <irifigueroa1408@gmail.com>
2025-08-28 12:45:38 -0300

    Update usuarios.routes.js

--- routes/usuarios.routes.js
+++ routes/usuarios.routes.js
@@ -1,28 +1,75 @@
 import { Router } from "express";
+import prisma from "../lib/prisma.js";
+
 const router = Router();
 
-router.get("/usuarios", (req, res) => {
-  res.json({ mensaje: "Esta es la ruta [get] de mi entidad [usuarios]" });
+router.get("/usuarios", async (req, res) => {
+  try {
+    const usuarios = await prisma.usuarios.findMany();
+    res.json(usuarios);
+  } catch (error) {
+    res.status(500).json({ error: "Hubo un error al traer los usuarios" });
+  }
 });
 
-router.get("/usuarios/:id", (req, res) => {
-  const id = req.params.id;
-  res.json({ mensaje: `Esta es la ruta GET de mi entidad [usuarios] con el ID ${id}` });
+router.get("/usuarios/:id", async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    const usuario = await prisma.usuarios.findUnique({
+      where: { id: id },
+    });
+    res.json(usuario);
+  } catch (error) {
+    res.status(500).json({ error: "Hubo un error al traer el usuario" });
+  }
 });
 
-router.post("/usuarios", (req, res) => {
-  res.json({ mensaje: "Esta es la ruta [post] de mi entidad [usuarios]" });
+router.post("/usuarios", async (req, res) => {
+  try {
+    const { nombre, apellido, dni } = req.body;
+    const nuevoUsuario = await prisma.usuarios.create({
+      data: {
+        nombre,
+        apellido,
+        dni,
+      },
+    });
+    res.json(nuevoUsuario);
+  } catch (error) {
+    res.status(500).json({ error: "Hubo un error al crear el usuario" });
+  }
 });
 
-router.put("/usuarios/:id", (req, res) => {
-  const id = req.params.id;
-  res.json({ mensaje: `Esta es la ruta [put] de mi entidad [usuarios] con el ID ${id}` });
+router.put("/usuarios/:id", async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    const { nombre, apellido, dni } = req.body;
+    const usuarioActualizado = await prisma.usuarios.update({
+      where: { id: id },
+      data: {
+        nombre,
+        apellido,
+        dni,
+      },
+    });
+    res.json(usuarioActualizado);
+  } catch (error) {
+    res.status(500).json({ error: "Hubo un error al actualizar el usuario" });
+  }
 });
 
-router.delete("/usuarios/:id", (req, res) => {
-  const id = req.params.id;
-  res.json({ mensaje: `Esta es la ruta [delete] de mi entidad [usuarios] con el ID ${id}` });
+router.delete("/usuarios/:id", async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    await prisma.usuarios.delete({
+      where: { id: id },
+    });
+    res.json({ mensaje: "Usuario eliminado" });
+  } catch (error) {
+    res.status(500).json({ error: "Hubo un error al eliminar el usuario" });
+  }
 });
 
 export default router;
+
 
